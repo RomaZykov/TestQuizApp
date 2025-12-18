47848051a4f2cf88d9fcf7596da9a60d57aa20bc
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-12-18 15:20:02 +0300

    Remerge network package

---
+++ app/src/main/java/com/example/dailyquiztest/data/model/network/NetworkQuizDataSource.kt
@@ -0,0 +1,48 @@
+package com.example.dailyquiztest.data.model.network
+
+import com.example.dailyquiztest.R
+import com.example.dailyquiztest.core.StringProvider
+import com.example.dailyquiztest.data.model.network.model.NetworkQuizQuestion
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+interface NetworkQuizDataSource {
+
+    suspend fun retrieveQuizQuestions(
+        amount: Int,
+        category: Int,
+        difficulty: String
+    ): Result<List<NetworkQuizQuestion>>
+
+    class Base @Inject constructor(
+        private val quizApi: QuizApi,
+        private val stringProvider: StringProvider
+    ) : NetworkQuizDataSource {
+        override suspend fun retrieveQuizQuestions(
+            amount: Int,
+            category: Int,
+            difficulty: String
+        ): Result<List<NetworkQuizQuestion>> {
+            return try {
+                val questionsResponse = quizApi.fetchQuizQuestions(amount, category, difficulty)
+                val code = questionsResponse.body()!!.responseCode
+                if (code != SUCCESS_CODE) {
+                    throw UnknownHostException(
+                        stringProvider.string(
+                            R.string.network_error_exception,
+                            code
+                        )
+                    )
+                }
+                val questions = questionsResponse.body()!!.result!!
+                Result.success(questions)
+            } catch (e: UnknownHostException) {
+                Result.failure(e)
+            }
+        }
+    }
+
+    companion object {
+        const val SUCCESS_CODE = 0
+    }
+}
---
+++ app/src/main/java/com/example/dailyquiztest/data/model/network/di/NetworkModule.kt
@@ -0,0 +1,14 @@
+package com.example.dailyquiztest.data.model.network.di
+
+import com.example.dailyquiztest.data.model.network.NetworkQuizDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class NetworkModule {
+    @Binds
+    abstract fun bindNetworkQuizDataSource(impl: NetworkQuizDataSource.Base): NetworkQuizDataSource
+}
