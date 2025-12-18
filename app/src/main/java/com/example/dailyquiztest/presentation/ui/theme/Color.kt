ccc8e27226f4895249889856333351c01f8de0b8
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-12-18 15:16:24 +0300

    Remerge theme in presentation package

---
+++ app/src/main/java/com/example/dailyquiztest/presentation/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.dailyquiztest.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF7067FF)
+val SecondaryBlue = Color(0xFFBCB7FF)
+val TertiaryBlue = Color(0xFF2B0063)
+val SecondaryWhite = Color(0xFFF3F3F3)
+val Black = Color(0xFF000000)
+val Grey = Color(0xFFBABABA)
+val Red = Color(0xFFE70000)
+val Green = Color(0xFF00AE3A)
+val Yellow = Color(0xFFFFB800)
