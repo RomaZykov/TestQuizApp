a0673bc0ffda313b9250d43274772357af901baf
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-12-18 15:24:18 +0300

    Remerge mainActivity + app files

---
+++ app/src/main/java/com/example/dailyquiztest/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.dailyquiztest.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.dailyquiztest.presentation.features.welcome.navigation.WelcomeRoute
+import com.example.dailyquiztest.presentation.main_navigation.MainNavigation
+import com.example.dailyquiztest.presentation.ui.theme.DailyQuizeTestTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DailyQuizeTestTheme {
+                MainNavigation(
+                    navController = rememberNavController(),
+                    startDestination = WelcomeRoute
+                )
+            }
+        }
+    }
+}
