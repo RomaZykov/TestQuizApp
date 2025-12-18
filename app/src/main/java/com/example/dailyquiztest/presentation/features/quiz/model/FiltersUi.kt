3f69a305001df21a836f5a2f9a527c2be7759c08
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-12-18 19:34:33 +0300

    Replace toast to snackBar

--- app/src/main/java/com/example/dailyquiztest/presentation/features/quiz/model/FiltersUi.kt
+++ app/src/main/java/com/example/dailyquiztest/presentation/features/quiz/model/FiltersUi.kt
@@ -1,10 +1,10 @@
 package com.example.dailyquiztest.presentation.features.quiz.model
 
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +18,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -103,7 +103,7 @@ data class FiltersUi(
                     )
                 }
                 if (shouldShowErrorToast.value) {
-                    DisplayToast()
+                    DisplaySnackBar()
                 }
             }
         }
@@ -124,10 +124,16 @@ data class FiltersUi(
     }
 
     @Composable
-    private fun DisplayToast() {
-        val context = LocalContext.current
-        Toast.makeText(context, stringResource(R.string.welcome_error_text), Toast.LENGTH_LONG)
-            .show()
+    private fun BoxScope.DisplaySnackBar() {
+        Snackbar(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(24.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Text(stringResource(R.string.error_message))
+        }
     }
 
     @Composable
@@ -280,7 +286,6 @@ data class FiltersUi(
 @Preview(showSystemUi = true)
 @Composable
 fun FiltersPreview() {
-    FiltersUi(CategoriesTypes.entries.toList(), emptyList(), false)
+    FiltersUi(CategoriesTypes.entries.toList(), emptyList(), true)
         .Display({ _, _ -> }, { _ -> }, {}, { _ -> }) {}
 }
-
