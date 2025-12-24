d1be2ed63e818421289c27f60520f1cae2c0f45e
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-12-24 17:30:43 +0300

    add exp opt for fake dispatchers for tests

--- testing/src/main/java/com/example/testing/di/FakeDispatcherList.kt
+++ testing/src/main/java/com/example/testing/di/FakeDispatcherList.kt
@@ -2,8 +2,10 @@ package com.example.testing.di
 
 import com.example.dailyquiztest.core.DispatcherList
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class FakeDispatcherList : DispatcherList {
     var wasIoCalled = false
     var wasUiCalled = false
