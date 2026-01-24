c01892faff40b74ef50de9d1f68c79745973787d
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2026-01-24 19:39:02 +0300

    add new UI tests

--- app/src/androidTest/java/com/example/dailyquiztest/ScenarioTest.kt
+++ app/src/androidTest/java/com/example/dailyquiztest/ScenarioTest.kt
@@ -1,30 +1,34 @@
 package com.example.dailyquiztest
 
-import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
+import android.content.pm.ActivityInfo
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeUp
 import com.example.dailyquiztest.core.StringResources
-import com.example.testing.dummy.dummyHistoryResults
 import com.example.dailyquiztest.domain.model.Category
 import com.example.dailyquiztest.domain.model.Difficulty
 import com.example.dailyquiztest.domain.repository.HistoryRepository
 import com.example.dailyquiztest.domain.repository.QuizRepository
-import com.example.dailyquiztest.pages.FiltersPage
-import com.example.dailyquiztest.pages.HistoryPage
-import com.example.dailyquiztest.pages.QuizPage
-import com.example.dailyquiztest.pages.ResultPage
-import com.example.dailyquiztest.pages.WelcomePage
-import com.example.dailyquiztest.presentation.features.welcome.navigation.WelcomeRoute
-import com.example.dailyquiztest.presentation.main_navigation.MainNavigation
-import com.example.dayliquiztest.HiltComponentActivity
+import com.example.dailyquiztest.helpPages.FiltersPage
+import com.example.dailyquiztest.helpPages.HistoryPage
+import com.example.dailyquiztest.helpPages.QuizPage
+import com.example.dailyquiztest.helpPages.ResultPage
+import com.example.dailyquiztest.helpPages.WelcomePage
+import com.example.dailyquiztest.presentation.MainActivity
+import com.example.dailyquiztest.presentation.features.history.HistoryUiState
+import com.example.dailyquiztest.presentation.features.quiz.QuizUiState
+import com.example.dailyquiztest.presentation.features.welcome.WelcomeUiState
+import com.example.testing.dummy.dummyHistoryResults
+import com.example.testing.dummy.stubQuizAnswers
+import com.example.testing.dummy.stubQuizes
 import com.example.testing.repository.FakeQuizRepository
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -34,10 +38,8 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import javax.inject.Inject
 
-@RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class ScenarioTest : StringResources() {
 
@@ -45,7 +47,7 @@ class ScenarioTest : StringResources() {
     val hiltRule = HiltAndroidRule(this)
 
     @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Inject
     lateinit var fakeHistoryRepository: HistoryRepository
@@ -62,8 +64,6 @@ class ScenarioTest : StringResources() {
     private lateinit var resultPage: ResultPage
     private lateinit var filtersPage: FiltersPage
 
-    private lateinit var navController: TestNavHostController
-
     @Before
     fun setUp() {
         hiltRule.inject()
@@ -72,13 +72,6 @@ class ScenarioTest : StringResources() {
         filtersPage = FiltersPage(composeTestRule)
         quizPage = QuizPage(composeTestRule, fakeQuizRepository)
         resultPage = ResultPage(composeTestRule)
-        composeTestRule.activity.setContent {
-            navController = rememberTestNavController()
-            MainNavigation(
-                navController = navController,
-                startDestination = WelcomeRoute
-            )
-        }
     }
 
     @Test
@@ -116,7 +109,7 @@ class ScenarioTest : StringResources() {
     }
 
     @Test
-    fun deleteAllHistories() = runTest {
+    fun deleteAllHistories_showsEmptyHistory() = runTest {
         welcomePage.assertPageDisplayed()
         welcomePage.clickHistoryButton()
 
@@ -180,7 +173,7 @@ class ScenarioTest : StringResources() {
             retrieveString(R.string.five_stars_title),
             retrieveString(R.string.five_stars_description)
         )
-        resultPage.clickStartAgainButton()
+        resultPage.clickTopStartAgainButton()
 
         welcomePage.assertPageDisplayed()
         welcomePage.clickHistoryButton()
@@ -326,18 +319,215 @@ class ScenarioTest : StringResources() {
             retrieveString(R.string.zero_stars_title),
             retrieveString(R.string.zero_stars_desc)
         )
-        resultPage.clickStartAgainButton()
+        resultPage.clickTopStartAgainButton()
 
         welcomePage.assertPageDisplayed()
     }
 
-    @Composable
-    private fun rememberTestNavController(): TestNavHostController {
-        val context = LocalContext.current
-        return remember {
-            TestNavHostController(context).apply {
-                navigatorProvider.addNavigator(ComposeNavigator())
+    // Horizontal test checks
+    @Test(timeout = 60000)
+    fun checkHorizontalLandscape_hasCorrectBehaviour_onWelcomeScreen() {
+        welcomePage.assertPageDisplayed()
+        welcomePage.assertHistoryButtonDisplayed()
+        welcomePage.assertStartQuizButtonDisplayed()
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithContentDescription(WelcomeUiState.INITIAL_WELCOME_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeUp()
+                }
             }
+        composeTestRule.waitForIdle()
+
+        welcomePage.assertPageDisplayed()
+        welcomePage.assertHistoryButtonNotDisplayed()
+        welcomePage.assertStartQuizButtonDisplayed()
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        composeTestRule.waitForIdle()
+
+        welcomePage.assertPageDisplayed()
+        welcomePage.assertHistoryButtonNotDisplayed()
+
+        composeTestRule.onNodeWithContentDescription(WelcomeUiState.INITIAL_WELCOME_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeDown()
+                }
+            }
+        composeTestRule.waitForIdle()
+
+        welcomePage.assertPageDisplayed()
+        welcomePage.assertHistoryButtonDisplayed()
+        welcomePage.assertStartQuizButtonDisplayed()
+    }
+
+    @Test(timeout = 60000)
+    fun checkHorizontalLandscape_hasCorrectBehaviour_onHistoryScreen() = runTest {
+        welcomePage.clickHistoryButton()
+
+        historyPage.initWithDummyHistories()
+        composeTestRule.waitForIdle()
+        historyPage.assertNonEmptyHistoriesDisplayed()
+        historyPage.assertHistoryTitleWithBackButtonDisplayed()
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        composeTestRule.waitForIdle()
+
+        historyPage.assertNonEmptyHistoriesDisplayed()
+        historyPage.assertHistoryTitleWithBackButtonDisplayed()
+
+        composeTestRule.onNodeWithContentDescription(HistoryUiState.NON_EMPTY_HISTORY_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeUp()
+                }
+            }
+        composeTestRule.waitForIdle()
+
+        historyPage.assertNonEmptyHistoriesDisplayed()
+        historyPage.assertHistoryTitleWithBackButtonNotDisplayed()
+        composeTestRule.onNodeWithText(
+            retrieveString(
+                R.string.quiz_number_title,
+                dummyHistoryResults.last().id + 1
+            )
+        ).assertExists().assertIsDisplayed()
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        composeTestRule.waitForIdle()
+
+        historyPage.assertNonEmptyHistoriesDisplayed()
+        historyPage.assertHistoryTitleWithBackButtonNotDisplayed()
+
+        composeTestRule.onNodeWithContentDescription(HistoryUiState.NON_EMPTY_HISTORY_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeDown()
+                }
+            }
+        composeTestRule.waitForIdle()
+
+        historyPage.assertNonEmptyHistoriesDisplayed()
+        historyPage.assertHistoryTitleWithBackButtonDisplayed()
+    }
+
+    @Test(timeout = 60000)
+    fun checkHorizontalLandscape_hasCorrectBehaviour_onFiltersScreen() {
+        welcomePage.clickStartButton()
+
+        filtersPage.assertPageDisplayed()
+        filtersPage.assertStartQuizButtonNotEnabled()
+        filtersPage.chooseSomeCategory(Category.FILM)
+        filtersPage.chooseSomeDifficulty(Difficulty.EASY)
+        filtersPage.assertStartQuizButtonEnabled()
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        composeTestRule.waitForIdle()
+
+        filtersPage.assertCategorySelected(Category.FILM)
+        filtersPage.assertDifficultySelected(Difficulty.EASY)
+        filtersPage.assertStartQuizButtonEnabled()
+
+        composeTestRule.onNodeWithContentDescription(QuizUiState.FILTERS_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeUp()
+                }
+            }
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithContentDescription(QuizUiState.FILTERS_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeDown()
+                }
+            }
+
+        filtersPage.assertCategorySelected(Category.FILM)
+        filtersPage.assertDifficultySelected(Difficulty.EASY)
+        filtersPage.assertStartQuizButtonEnabled()
+    }
+
+    @Test(timeout = 60000)
+    fun checkHorizontalLandscape_hasCorrectBehaviour_onQuizScreen() {
+        welcomePage.clickStartButton()
+        filtersPage.chooseSomeCategory(Category.FILM)
+        filtersPage.chooseSomeDifficulty(Difficulty.EASY)
+        filtersPage.clickStartQuizButton()
+
+        quizPage.assertPageDisplayed()
+        quizPage.assertNextButtonNotEnabled()
+        composeTestRule.onNodeWithText(stubQuizes.first().incorrectAnswers[2])
+            .performScrollTo()
+            .performClick()
+        composeTestRule.onNodeWithText(stubQuizes.first().incorrectAnswers[0])
+            .performScrollTo()
+            .performClick()
+        quizPage.assertNextButtonEnabled()
+
+        composeTestRule.onNodeWithContentDescription(QuizUiState.QUIZ_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeUp()
+                }
+            }
+
+        composeTestRule.onNodeWithContentDescription(QuizUiState.QUIZ_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeDown()
+                }
+            }
+
+        quizPage.assertPageDisplayed()
+        composeTestRule.onNodeWithText(stubQuizes.first().incorrectAnswers[2]).assertIsSelected()
+        composeTestRule.onNodeWithText(stubQuizes.first().incorrectAnswers[0]).assertIsSelected()
+        quizPage.assertNextButtonEnabled()
+    }
+
+    @Test(timeout = 60000)
+    fun checkHorizontalLandscape_hasCorrectBehaviour_onQuizResultScreen() {
+        welcomePage.clickStartButton()
+        filtersPage.chooseSomeCategory(Category.TELEVISION)
+        filtersPage.chooseSomeDifficulty(Difficulty.MEDIUM)
+        filtersPage.clickStartQuizButton()
+        // 10 for MEDIUM
+        val questionsSize = Difficulty.MEDIUM.amountOfQuestions
+        repeat(questionsSize - 1) {
+            quizPage.chooseOption(it % 2 == 0)
+            quizPage.clickNextButton()
         }
+        quizPage.chooseOption(true)
+        quizPage.clickFinishQuizButton()
+
+        resultPage.assertPageDisplayed()
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithContentDescription(QuizUiState.RESULTS_SCREEN)
+            .performTouchInput {
+                repeat(5) {
+                    swipeUp()
+                }
+            }
+        composeTestRule.waitForIdle()
+
+        resultPage.performScrollToItemWithText(stubQuizAnswers[4].question)
+        resultPage.performScrollToItemWithText(stubQuizAnswers[9].question)
+
+        composeTestRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithText(stubQuizes[9].question).assertIsDisplayed()
+        resultPage.clickBottomStartAgainButton()
+
+        welcomePage.assertPageDisplayed()
     }
 }
