84f9248e57d5236d0e243d5d64b62d9df50a16fd
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2026-02-11 23:38:55 +0300

    refactor unit test

--- app/src/test/java/com/example/dailyquiztest/presentation/QuizViewModelTest.kt
+++ app/src/test/java/com/example/dailyquiztest/presentation/QuizViewModelTest.kt
@@ -44,6 +44,7 @@ class QuizViewModelTest {
     private lateinit var viewModel: QuizViewModel
     private lateinit var stateFlow: StateFlow<QuizUiState>
     private lateinit var score: CalculateScore.All
+    private lateinit var mapper: QuizMapper
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private var testDispatcher = UnconfinedTestDispatcher()
@@ -58,6 +59,7 @@ class QuizViewModelTest {
         fakeWelcomeRouteProvider = FakeWelcomeRouteProvider()
         dispatchers = FakeDispatcherList(testDispatcher)
         score = CalculateScore.Base()
+        mapper = QuizMapper.Base()
         viewModel = QuizViewModel(
             quizRepository = fakeQuizRepository,
             historyRepository = fakeHistoryRepository,
@@ -65,7 +67,7 @@ class QuizViewModelTest {
             dispatcherList = dispatchers,
             score = score,
             formatDate = fakeFormatDate,
-            mapper = QuizMapper.Base()
+            mapper = mapper
         )
         stateFlow = viewModel.uiState
     }
@@ -95,16 +97,19 @@ class QuizViewModelTest {
             )
             advanceTimeBy(505)
 
+            val question = "Test question 1"
+            val incorrectAnswers = listOf("InCorrect 1", "InCorrect 2", "InCorrect 3")
+            val correctAnswer = "Correct"
             val expectedFinalState = QuizUi(
                 number = 1,
-                question = stubDomainQuizes[0].question,
-                incorrectAnswers = stubDomainQuizes[0].incorrectAnswers,
-                correctAnswer = stubDomainQuizes[0].correctAnswer,
+                question = question,
+                incorrectAnswers = incorrectAnswers,
+                correctAnswer = correctAnswer,
                 totalQuestions = stubDomainQuizes.size,
                 quizGroupUi = QuizGroupUi.MultipleGroupUi(
-                    question = stubDomainQuizes[0].question,
-                    correctOption = stubDomainQuizes[0].correctAnswer,
-                    inCorrectOptions = stubDomainQuizes[0].incorrectAnswers,
+                    question = question,
+                    correctOption = correctAnswer,
+                    inCorrectOptions = incorrectAnswers,
                     userAnswer = ""
                 )
             )
@@ -150,14 +155,14 @@ class QuizViewModelTest {
         runTest {
             fakeQuizRepository.shouldSimulateOnlyTrueFalseOptions = true
 
-            var currentNumQuestion = 0
+            var curIndex = 0
             val expectedQuizResults = mutableListOf<QuizUi>()
             viewModel.prepareQuizGame(
                 CategoryDomain.CARTOON_AND_ANIMATIONS,
                 DifficultyDomain.EASY
             )
             repeat(4) {
-                val currentQuestion = retrieveDummyTrueFalseQuestionByIndex(currentNumQuestion)
+                val currentQuestion = retrieveDummyTrueFalseQuestionByIndex(curIndex, 5)
                 assertEquals(currentQuestion, stateFlow.value)
 
                 val correctAnsweredQuestion = currentQuestion.copy(
@@ -166,12 +171,12 @@ class QuizViewModelTest {
                 )
                 viewModel.saveQuizAnswer(correctAnsweredQuestion)
                 expectedQuizResults.add(correctAnsweredQuestion)
-                currentNumQuestion++
+                curIndex++
 
                 viewModel.retrieveNextAnswer()
             }
 
-            val finalQuestion = retrieveDummyTrueFalseQuestionByIndex(currentNumQuestion)
+            val finalQuestion = retrieveDummyTrueFalseQuestionByIndex(curIndex, 5)
             assertEquals(finalQuestion, stateFlow.value)
 
             val incorrectAnsweredQuestion = finalQuestion.copy(
@@ -186,19 +191,25 @@ class QuizViewModelTest {
             assertEquals(expectedResultUiState, stateFlow.value)
         }
 
-    private fun retrieveDummyTrueFalseQuestionByIndex(index: Int): QuizUi {
+    private fun retrieveDummyTrueFalseQuestionByIndex(index: Int, totalQuestions: Int): QuizUi {
+        val question = stubTrueFalseQuizes[index].question.replaceFirstChar { it.uppercaseChar() }
+        val incorrectAnswers = stubTrueFalseQuizes[index].incorrectAnswers.map { word ->
+            word.replaceFirstChar { it.uppercaseChar() }
+        }
+        val correctAnswer = stubTrueFalseQuizes[index].correctAnswer.replaceFirstChar { it.uppercaseChar() }
+        val userAnswer = stubTrueFalseQuizes[index].userAnswer.replaceFirstChar { it.uppercaseChar() }
         return QuizUi(
-            number = index,
-            question = stubTrueFalseQuizes[index].question,
-            incorrectAnswers = stubTrueFalseQuizes[index].incorrectAnswers,
-            correctAnswer = stubTrueFalseQuizes[index].correctAnswer,
-            totalQuestions = stubTrueFalseQuizes.size,
-            userAnswer = stubTrueFalseQuizes[index].userAnswer,
+            number = index + 1,
+            question = question,
+            incorrectAnswers = incorrectAnswers,
+            correctAnswer = correctAnswer,
+            totalQuestions = totalQuestions,
+            userAnswer = userAnswer,
             isAnsweredCorrect = stubTrueFalseQuizes[index].isAnsweredCorrect,
             quizGroupUi = QuizGroupUi.BooleanGroupUi(
-                question = stubTrueFalseQuizes[index].question,
-                correctOption = stubTrueFalseQuizes[index].correctAnswer,
-                userAnswer = stubTrueFalseQuizes[index].userAnswer
+                question = question,
+                correctOption = correctAnswer,
+                userAnswer = userAnswer
             )
         )
     }
