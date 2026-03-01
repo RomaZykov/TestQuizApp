5c3a83f898f0ca4e16dee62a22a9fecaa7995eec
Roman Zykov <zyckoff.rom@yandex.ru>
2026-03-02 00:04:53 +0300

    bump version to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.dailyquizetest"
         minSdk = 26
         targetSdk = 33
-        versionCode = 11
-        versionName = "1.11"
+        versionCode = 12
+        versionName = "1.12"
 
         testInstrumentationRunner = "com.example.dailyquiztest.core.DailyQuizTestRunner"
     }
