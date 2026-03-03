74757aba6eb2b81b501ecf1cfec5d53129c85ee2
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2026-03-03 19:27:03 +0300

    add snackBar
    With context between EmptyHistoryUi and HistoryUi, when historyUi has changed to EmptyUi we should show snackBar little time

--- app/src/main/java/com/example/dailyquiztest/presentation/feature/history/HistoryScreen.kt
+++ app/src/main/java/com/example/dailyquiztest/presentation/feature/history/HistoryScreen.kt
@@ -1,13 +1,38 @@
 package com.example.dailyquiztest.presentation.feature.history
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarVisuals
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.dailyquiztest.R
 import com.example.dailyquiztest.presentation.main_navigation.Route
 import com.example.dailyquiztest.presentation.main_navigation.navigateIfResumed
+import kotlinx.coroutines.launch
 
 @Composable
 fun HistoryScreenNav(
@@ -35,6 +60,9 @@ fun HistoryScreen(
     navigateToFilters: (toFilters: (Route) -> Unit) -> Unit,
     deleteQuizHistory: (quizNumber: Int) -> Unit
 ) {
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val message = stringResource(R.string.delete_retry)
     uiState.Display(
         historyUserActions = object : HistoryUserActions {
             override fun onBackButtonClicked(): () -> Unit = {
@@ -43,6 +71,9 @@ fun HistoryScreen(
 
             override fun onDeleteClicked(): (Int) -> Unit = {
                 deleteQuizHistory.invoke(it)
+                scope.launch {
+                    snackBarHostState.showSnackbar(message)
+                }
             }
 
             override fun onStartQuizClicked(): () -> Unit = {
@@ -50,4 +81,54 @@ fun HistoryScreen(
             }
         }
     )
+    Box {
+        SnackbarHost(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            hostState = snackBarHostState
+        ) {
+            CustomSnackBar(it)
+        }
+    }
+}
+
+@Composable
+private fun CustomSnackBar(data: SnackbarData) {
+    val snackBarContDesc = stringResource(R.string.snack_bar_cont_desc)
+    Snackbar(
+        modifier = Modifier
+            .semantics {
+                contentDescription = snackBarContDesc
+            }
+            .padding(32.dp),
+        shape = RoundedCornerShape(32.dp),
+        containerColor = Color.White
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = data.visuals.message,
+            color = Color.Black,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SnackBarPreview() {
+    CustomSnackBar(object : SnackbarData {
+        override fun dismiss() {}
+        override fun performAction() {}
+        override val visuals: SnackbarVisuals
+            get() = object : SnackbarVisuals {
+                override val actionLabel: String?
+                    get() = null
+                override val duration: SnackbarDuration
+                    get() = TODO("Not yet implemented")
+                override val message: String
+                    get() = "Test message"
+                override val withDismissAction: Boolean
+                    get() = TODO("Not yet implemented")
+            }
+    })
 }
